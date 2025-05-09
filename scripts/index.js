19c0e29152f222ac3c0d0f8e9c6821ee82204c6c
Tarriebrown1 <tkbrown02@yahoo.com>
2025-05-09 15:37:57 -0400

    New update for my project 8

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,25 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__new-post-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal__is-opened");
 });
 
@@ -21,3 +34,10 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal__is-opened");
 });
+
+function handleEditProfileFormSubmit(evt) {
+  evt.preventDefault();
+  console.log("submitting");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileFormSubmit());
